8e41c475069b870f16312a72401732fa5794c507
thakursaahil99 <sahilthakur961999@gmail.com>
2026-07-22 16:30:26 +0530

    metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 import LenisProvider from "@/components/providers/LenisProvider";
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Sahil Thakur| Full-Stack Developer",
+  description:
+    "Available for hiring.",
+}
 
 export default function RootLayout({
   children,
